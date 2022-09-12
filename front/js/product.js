cb76ae49827d1072c919516226f25fac178fd941
Kevin Fernandes <17kevinf4@gmail.com>
2022-09-12 10:21:16 +0200

    MAJ Fiche produits

--- front/js/product.js
+++ front/js/product.js
@@ -14,15 +14,12 @@ fetch("http://localhost:3000/api/products/" + id)
 
   console.log(sofa);    
 
-  // Récuperer l'élément de l'image
+  // On récupère l'élément de l'image //
     const item__img = document.getElementsByClassName("item__img")[0];
     const imageSofa = document.createElement("img");
     imageSofa.src = sofa.imageUrl;
     imageSofa.alt = sofa.imageAltTxt;
     item__img.appendChild(imageSofa);
-
-  
-    //imageSofa[0].appendChild(imageSofa);
 
     let items = document.getElementById ("title");
     items.innerHTML = sofa.name;
@@ -31,7 +28,7 @@ fetch("http://localhost:3000/api/products/" + id)
     priceSofa.innerHTML = sofa.price;
     
     for (let i=0; i < sofa.colors.length; i++) {
-    let colorsSofa = document.getElementById ("colors");
+    let colorsSofa = document.getElementById("colors");
     let option = document.createElement("option");
     option.value = sofa.option;
     option.textContent = sofa.colors[i];
@@ -41,3 +38,33 @@ fetch("http://localhost:3000/api/products/" + id)
     descriptionSofa.innerHTML = sofa.description;
 
     }})
+
+console.log("Les caractéristiques des produits sont affichées !");   
+
+    let colorChoices = document.querySelector ("#colors");
+    let sofaQuantity = document.querySelector ("#quantity");
+
+    let clickbutton = document.querySelector("#addToCart");
+    clickbutton.addEventListener('click', function () {
+    
+      // Création de la liste des infos des produits à stocker dans le LocalStorage //
+     let productOptions = {
+     colors: colorChoices.value,
+     quantity: sofaQuantity.value,
+     id: id,
+    };
+
+      // Obligation de choisir une couleur et un nombre entre 1 et 100 //
+    if (productOptions.colors === "") {
+      alert ("Une couleur doit être sélectionnée !")
+    } else if (quantity.value > 100 && quantity.value < 1);
+
+     // Récupérer l'intégralité des produits dans le LocalStorage //
+     console.log (productOptions);
+     localStorage.setItem ("id", productOptions.id);
+     localStorage.setItem ("colors", productOptions.colors);
+     localStorage.setItem ("quantity", productOptions.quantity);
+     console.log (localStorage);
+})
+    
+    
